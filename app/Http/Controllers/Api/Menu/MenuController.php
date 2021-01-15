f932973b4d65910d5036370de798128eef19dd04
luis.salazar@kfc.com.ec <luis.salazar@kfc.com.ec>
2021-01-15 15:59:08 -0500

    funnciona

--- app/Http/Controllers/Api/Menu/MenuController.php
+++ app/Http/Controllers/Api/Menu/MenuController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 use App\Services\UsersPosService;
+use Illuminate\Support\Facades\Config;
 
 class MenuController extends Controller
 {
