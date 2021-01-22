e68dcc2744a6c750c7944f1d64da1f0f76a7cf86
luis.salazar@kfc.com.ec <luis.salazar@kfc.com.ec>
2021-01-22 13:25:02 -0500

    new factura payload

--- app/Http/Controllers/Api/FacturaPayload/FacturaPayloadController.php
+++ app/Http/Controllers/Api/FacturaPayload/FacturaPayloadController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers\Api\FacturaPayload;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\FacturaPayload;
 
 class FacturaPayloadController extends Controller
 {
     public function get(Request $request, $pais) {
+
         return $pais . ' get ';
     }
 
     public function post(Request $request, $pais) {
-        return $pais . ' post ';
+        $data = $request->json()->all();
+        $new_factura_payload = new FacturaPayload();
+        $new_factura_payload->orden = $data['orden'];
+        $new_factura_payload->IDCabeceraFactura = $data['IDCabeceraFactura'];
+        $new_factura_payload->save();
+        return $new_factura_payload;
     }
 
     public function put(Request $request, $pais) {
