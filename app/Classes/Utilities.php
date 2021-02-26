1c8031fd915d9536037a3ae5b0951077fb1f22f8
pasthortown <pastorchessmaster@gmail.com>
2021-02-26 15:28:12 -0500

    utilites

--- app/Classes/Utilities.php
+++ app/Classes/Utilities.php
@@ -38,4 +38,41 @@ class Utilities
         }
         return $keys;
     }
+
+    public function httpGet($url, $data=NULL) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if(!empty($data)){
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+        $headersSend = array('Content-Type: application/json');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headersSend);
+        $response = curl_exec($ch);
+        if (curl_error($ch)) {
+            trigger_error('Curl Error:' . curl_error($ch));
+        }
+        curl_close($ch);
+        return $response;
+    }
+
+    public function httpPost($url, $data=NULL) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        if(!empty($data)){
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+        $headersSend = array('Content-Type: application/json');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headersSend);
+        $response = curl_exec($ch);
+        if (curl_error($ch)) {
+            trigger_error('Curl Error:' . curl_error($ch));
+        }
+        curl_close($ch);
+        return $response;
+    }
 }
