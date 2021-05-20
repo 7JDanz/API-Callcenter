51604deb47ea7fd3e6ec515a35db056b1eefabdc
pasthortown <pastorchessmaster@gmail.com>
2021-05-20 16:59:32 -0500

    correccion producto

--- app/Classes/MenuUtil.php
+++ app/Classes/MenuUtil.php
@@ -164,6 +164,28 @@ class MenuUtil
                     }
                 }
             }
+
+            foreach($payload->MenuCategorias as $menu_categorias) {
+                if (is_array($menu_categorias->productos)) {
+                    foreach($menu_categorias->productos as $producto) {
+                        array_push($plus, $producto->IDProducto);
+                        if (isset($producto->Preguntas)) {
+                            if (is_array($producto->Preguntas)) {
+                                foreach($producto->Preguntas as $pregunta) {
+                                    if (isset($pregunta->Respuestas)) {
+                                        if (is_array($pregunta->Respuestas)) {
+                                            foreach($pregunta->Respuestas as $respuesta) {
+                                                array_push($plus, $respuesta->IDProducto);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
         }
 
         return join(',',$plus);
