c9da896f3cd69633f94e4e695c1cf1c9d08713b5
pasthortown <pastorchessmaster@gmail.com>
2021-05-20 10:33:03 -0500

    prueba a

--- app/Http/Controllers/Api/Menu/MenuController.php
+++ app/Http/Controllers/Api/Menu/MenuController.php
@@ -73,8 +73,8 @@ class MenuController extends Controller
             \Cache::put('plus_'.$menu, $plus_filter, 3600);
         } else {
             $menuPayload = \Cache::get($menu);
+            return $menuPayload;
             $plus_filter = $menu_util->get_productos_menu($menuPayload);
-            return $plus_filter;
             //$plus_filter = \Cache::get('plus_'.$menu);
         }
         $sql_query = "select * from config.fn_buscaPreciosxPlu ($restaurante,'$plus_filter')";
